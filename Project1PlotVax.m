5320ea6dbafba160adc4594205c48960197d8f4e
Sunsprint <63557127+Sunsprint@users.noreply.github.com>
2020-10-05 10:43:43 -0400

    Add files via upload

---
+++ Project1PlotVax.m
@@ -0,0 +1,47 @@
+% Wave 1 - Mutagen v.1
+% Defines Rates of Transfer (Flows)
+beta = 1/90; % Infection Rate
+gamma = 1/2; % Recovery rate in 1/weeks
+
+i_0 = 2; % Number of initial infections
+population = 100;
+s_0 = population - i_0;
+r_0 = 0; 
+
+% Vaccination Setup
+vaccine = 50/100;
+dose = 5/100;
+
+num_steps = 40; % Number of Days Simulated
+
+[S,I,R,W]=sir_v1_simulate_vax(s_0, i_0, r_0, beta, gamma, num_steps, vaccine, dose);
+
+figure(1); clf; hold on;
+plot(W,I,'k-'); label1 = "Wave 1";
+
+% Wave 2 - Mutagen v.2
+
+num_steps = 40;
+
+% Defines Rates of Transfer (Flows)
+delta = 1/90; % Infection Rate
+epsilon = 1/4; % Recovery rate in 1/weeks
+
+i2_0 = 0; % Number of initial infections
+s2_0 = 0;
+r2_0 = 0; 
+
+% Vaccination Setup
+vaccine2 = 50/100;
+dose2 = 5/100;
+
+m = 10; % Number of Days Simulated
+
+s2_m = 99; %number of susceptible individuals once the virus mutates (v2)
+i2_m = 1;  %number of infected individuals once the virus mutates(v2)
+r2_m = 0;  %number of recovered individuals once the virus mutates(v2)
+
+[S2,I2,R2,W]=sir_v2_simulate_vax(s2_0, i2_0, r2_0, delta, epsilon, num_steps, m, s2_m, i2_m, r2_m, vaccine, dose);
+
+plot(W,I2,'o-'); label2 = "Wave 2";
+
---
+++ sir_v2_simulate_vax.m
@@ -0,0 +1,62 @@
+function [S2, I2, R2, W] = sir_v2_simulate_vax(s2_0, i2_0, r2_0, delta, epsilon, num_steps, m, s2_m, i2_m, r2_m, vaccine2, dose2)
+% fcn_simulate Simulate the SIR model
+% Arguments
+%
+%   s2_0 = initial number of susceptible individuals (v2) (none since it
+%   doesn't exist yet)
+%   i2_0 = initial number of infected individuals(v2)(none since it
+%   doesn't exist yet)
+%   r2_0 = initial number of recovered individuals(v2)(none since it
+%   doesn't exist yet)
+%   
+%   s2_m = number of susceptible individuals once the virus mutates(v2)
+%   i2_m = number of infected individuals once the virus mutates(v2)
+%   r2_m = number of recovered individuals once the virus mutates(v2)
+%
+%   delta = infection rate (v2) parameter
+%   epsilon = recovery rate (v2) parameter
+%
+%   num_steps = number of simulation steps to simulate (how many weeks to
+%   model)
+%
+%   m = number of weeks it takes until v1 mutates into v2
+%
+% Returns
+%   S2 = simulation history of susceptible individuals (version 2); vector
+%   I2 = simulation history of infected individuals (version 2); vector
+%   R2 = simulation history of recovered individuals (version 2); vector
+%   W = simulation week; vector
+
+% Setup
+S2 = zeros(1, num_steps);
+I2 = zeros(1, num_steps);
+R2 = zeros(1, num_steps);
+W = 1 : num_steps;
+
+%s2 = s2_0;
+%s2 = s2_m; 
+
+% Store initial values
+%S2(1) = s2;
+%I2(1) = i2;
+%R2(1) = r2;
+S2(1) = s2_0;
+I2(1) = i2_0;
+R2(1) = r2_0;
+
+% number of susceptible individuals at time of initial mutation is the # of people recovered from v1 + the # of people susceptible to v1 
+% NOTE: This assumes people infected with v1 can become infected with v2(no one is safe)
+s2 = s2_m
+i2 = i2_m
+r2 = r2_m
+
+
+% Run simulation
+for step = m : num_steps
+    [s2, i2, r2] = sir_v2_step_vax(s2, i2, r2, delta, epsilon, vaccine2, dose2);
+    S2(step) = s2;
+    I2(step) = i2;
+    R2(step) = r2;
+end
+
+end
